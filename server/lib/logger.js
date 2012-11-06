1c9a92967abdb1a758f8599eca396e0c62cab38c
Spike Brehm <ocelot@gmail.com>
2012-11-05 19:51:43 -0800

    using Winston as logger

--- server/lib/logger.js
+++ server/lib/logger.js
@@ -1,58 +1,10 @@
 /**
   Plug in desired logger library and functionality here -- use this as an interface
 */
+var winston = require('winston');
 
-var _ = require('underscore');
+var logger = new winston.Logger({
+  transports: [new winston.transports.Console()]
+});
 
-var logLevelMap = {
-  'debug':0,
-  'info':1,
-  'warn':2,
-  'error':3,
-  'fatal':4
-}
-
-var config = {
-  stdioEnabled: true
-}
-
-module.exports.init = function init(options) {
-  if (config) {
-    _.extend(config, options);
-  }
-  // convert log level to int
-  config.logLevel = logLevelMap[config.logLevel];
-}
-
-module.exports.debug = function debug(msg) {
-  logMessage('debug', msg);
-}
-
-module.exports.info = function info(msg) {
-  logMessage('info', msg);
-}
-
-module.exports.warn = function warn(msg) {
-  logMessage('warn', msg);
-}
-
-module.exports.error = function error(msg) {
-  logMessage('error', msg);
-}
-
-module.exports.fatal = function fatal(msg) {
-  logMessage('fatal', msg);
-}
-
-
-module.exports.flush = function flush(callback) {
-  return callback();
-}
-
-function logMessage(logLevel, msg) {
-  if (logLevelMap[logLevel] >= config.logLevel) {
-    if (config.stdioEnabled) {
-      console.log("%s: %s", logLevel.toUpperCase(), msg);
-    }
-  }
-}
+module.exports = logger;

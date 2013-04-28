874f2fa25053d68f2f5e1004a63bf7b3127868d9
Spike Brehm <ocelot@gmail.com>
2013-04-27 22:11:27 -0700

    Using Backbone-style inheritance for classes where possible

--- shared/app.js
+++ shared/app.js
@@ -3,9 +3,7 @@
 * This is the app instance that is shared between client and server.
 * The client also subclasses it for client-specific stuff.
 */
-var Backbone, ClientRouter, Fetcher,
-  __hasProp = {}.hasOwnProperty,
-  __extends = function(child, parent) { for (var key in parent) { if (__hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor(); child.__super__ = parent.prototype; return child; };
+var Backbone, ClientRouter, Fetcher;
 
 require('./globals');
 Backbone = require('backbone');
@@ -17,21 +15,16 @@ if (!global.isServer) {
 
 function noop() {}
 
-module.exports = (function(_super) {
-  __extends(App, _super);
+module.exports = Backbone.Model.extend({
 
-  function App() {
-    App.__super__.constructor.apply(this, arguments);
-  }
-
-  App.prototype.defaults = {
+  defaults: {
     loading: false
-  };
+  },
 
   /*
   * @shared
   */
-  App.prototype.initialize = function() {
+  initialize: function() {
     this.fetcher = new Fetcher({
       app: this
     });
@@ -41,31 +34,28 @@ module.exports = (function(_super) {
       });
     }
     this.postInitialize();
-  };
+  },
 
-  App.prototype.postInitialize = noop;
+  postInitialize: noop,
 
   /*
   * @shared
   */
-  App.prototype.fetch = function() {
+  fetch: function() {
     this.fetcher.fetch.apply(this.fetcher, arguments);
-  };
+  },
 
   /*
   * @client
   */
-  App.prototype.bootstrapData = function(modelMap) {
+  bootstrapData: function(modelMap) {
     this.fetcher.bootstrapData(modelMap);
-  };
+  },
 
   /*
   * @client
   */
-  App.prototype.start = function() {
+  start: function() {
     this.router.start();
-  };
-
-  return App;
-
-})(Backbone.Model);
+  }
+});
--- shared/base/app_view.js
+++ shared/base/app_view.js
@@ -1,25 +1,16 @@
-var AppView, BaseView, hasPushState, _,
-  __hasProp = {}.hasOwnProperty,
-  __extends = function(child, parent) { for (var key in parent) { if (__hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor(); child.__super__ = parent.prototype; return child; };
+var BaseView, hasPushState, _;
 
 _ = require('underscore');
 BaseView = require('./view');
 
 hasPushState = typeof window !== "undefined" && window.history.pushState != null;
 
-module.exports = AppView = (function(_super) {
-  __extends(AppView, _super);
+module.exports = BaseView.extend({
+  el: 'body',
 
-  function AppView() {
-    this._interceptClick = _.bind(this._interceptClick, this);
-    AppView.__super__.constructor.apply(this, arguments);
-  }
+  initialize: function() {
+    BaseView.prototype.initialize.apply(this, arguments);
 
-  AppView.prototype.el = 'body';
-
-  AppView.prototype.initialize = function() {
-    AppView.__super__.initialize.apply(this, arguments);
-    this.app = this.options.app;
     _.defaults(this.options, {
       contentEl: '#content'
     });
@@ -29,20 +20,21 @@ module.exports = AppView = (function(_super) {
     */
     this.$content = $(this.options.contentEl);
     this._bindInterceptClick();
-  };
+    this._interceptClick = _.bind(this._interceptClick, this);
+  },
 
-  AppView.prototype.render = function() {};
+  render: function() {},
 
-  AppView.prototype.setCurrentView = function(view) {
+  setCurrentView: function(view) {
     this.$content.html(view.el);
     view.render();
-  };
+  },
 
-  AppView.prototype._bindInterceptClick = function() {
+  _bindInterceptClick: function() {
     this.$el.on('click', 'a:not([data-pass-thru])', this._interceptClick);
-  };
+  },
 
-  AppView.prototype._interceptClick = function(e) {
+  _interceptClick: function(e) {
     /*
     * We want the actual value of the attribute, rather than the
     * full URL, so we use jQuery instead of just e.currentTarget.href
@@ -54,9 +46,9 @@ module.exports = AppView = (function(_super) {
       e.preventDefault();
       this.app.router.redirectTo(href);
     }
-  };
+  },
 
-  AppView.prototype.shouldInterceptClick = function(href, el) {
+  shouldInterceptClick: function(href, el) {
     var hashParts, isHashClick;
 
     if (!(href && hasPushState)) {
@@ -65,8 +57,6 @@ module.exports = AppView = (function(_super) {
     hashParts = href.split('#');
     isHashClick = hashParts.length > 1 && hashParts[0] === window.location.pathname;
     return !isHashClick && href.slice(0, 1) === '/' && href.slice(0, 2) !== '//';
-  };
+  }
 
-  return AppView;
-
-})(BaseView);
+});

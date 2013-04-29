394afdf8e595313a30666c971bc8bc28a8d3cb9b
Spike Brehm <ocelot@gmail.com>
2013-04-29 09:46:10 -0700

    Updating ModelStore, CollectionStore to use inherit-component for inheritance

--- shared/store/collection_store.js
+++ shared/store/collection_store.js
@@ -1,62 +1,58 @@
-var CollectionStore, MemoryStore, getStoreKey, modelUtils, _,
-  __hasProp = {}.hasOwnProperty,
-  __extends = function(child, parent) { for (var key in parent) { if (__hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor(); child.__super__ = parent.prototype; return child; };
+var MemoryStore, Super, modelUtils, _, inherit;
 
+inherit = require('inherit-component');
 _ = require('underscore');
-MemoryStore = require('./memory_store');
+Super = MemoryStore = require('./memory_store');
 modelUtils = require('../modelUtils');
 
-module.exports = CollectionStore = (function(_super) {
-  __extends(CollectionStore, _super);
+module.exports = CollectionStore;
 
-  function CollectionStore() {
-    CollectionStore.__super__.constructor.apply(this, arguments);
-  }
+function CollectionStore() {
+  Super.apply(this, arguments);
+}
 
-  CollectionStore.prototype.set = function(collection, params) {
-    var data, idAttribute, key;
-    params = params || collection.params;
-    key = getStoreKey(modelUtils.modelName(collection.constructor), params);
-    idAttribute = collection.model.prototype.idAttribute;
-    data = {
-      ids: collection.pluck(idAttribute),
-      meta: collection.meta
-    };
-    return CollectionStore.__super__.set.call(this, key, data, null);
+inherit(CollectionStore, Super);
+
+CollectionStore.prototype.set = function(collection, params) {
+  var data, idAttribute, key;
+  params = params || collection.params;
+  key = getStoreKey(modelUtils.modelName(collection.constructor), params);
+  idAttribute = collection.model.prototype.idAttribute;
+  data = {
+    ids: collection.pluck(idAttribute),
+    meta: collection.meta
   };
+  return Super.prototype.set.call(this, key, data, null);
+};
+
+/*
+* Returns an array of model ids.
+*/
+CollectionStore.prototype.get = function(collectionName, params) {
+  var Collection, key;
+
+  params = params || {};
 
   /*
-  * Returns an array of model ids.
+  * Kind of jank-sauce. Always merge in the default
+  * params for the given collection.
   */
-  CollectionStore.prototype.get = function(collectionName, params) {
-    var Collection, key;
+  Collection = modelUtils.getCollectionConstructor(collectionName);
+  params = _.clone(params);
+  params = _.defaults(params, Collection.prototype.defaultParams);
+  key = getStoreKey(collectionName, params);
+  return Super.prototype.get.call(this, key);
+};
 
-    params = params || {};
+CollectionStore.prototype._formatKey = function(key) {
+  return Super.prototype._formatKey.call(this, "_cs:" + key);
+};
 
-    /*
-    * Kind of jank-sauce. Always merge in the default
-    * params for the given collection.
-    */
-    Collection = modelUtils.getCollectionConstructor(collectionName);
-    params = _.clone(params);
-    params = _.defaults(params, Collection.prototype.defaultParams);
-    key = getStoreKey(collectionName, params);
-    return CollectionStore.__super__.get.call(this, key);
-  };
-
-  CollectionStore.prototype._formatKey = function(key) {
-    return CollectionStore.__super__._formatKey.call(this, "_cs:" + key);
-  };
-
-  return CollectionStore;
-
-})(MemoryStore);
-
-getStoreKey = function(collectionName, params) {
+function getStoreKey(collectionName, params) {
   var underscored;
   underscored = modelUtils.underscorize(collectionName);
-  return "" + underscored + ":" + JSON.stringify(sortParams(params));
-};
+  return underscored + ":" + JSON.stringify(sortParams(params));
+}
 
 function sortParams(params) {
   var sorted = {};
--- shared/store/model_store.js
+++ shared/store/model_store.js
@@ -1,63 +1,59 @@
-var MemoryStore, ModelStore, modelUtils, _,
-  __hasProp = {}.hasOwnProperty,
-  __extends = function(child, parent) { for (var key in parent) { if (__hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor(); child.__super__ = parent.prototype; return child; };
+var MemoryStore, Super, modelUtils, _, inherit;
 
+inherit = require('inherit-component');
 _ = require('underscore');
-MemoryStore = require('./memory_store');
+Super = MemoryStore = require('./memory_store');
 modelUtils = require('../modelUtils');
 
-module.exports = ModelStore = (function(_super) {
-  __extends(ModelStore, _super);
+module.exports = ModelStore;
 
-  function ModelStore() {
-    ModelStore.__super__.constructor.apply(this, arguments);
+function ModelStore() {
+  Super.apply(this, arguments);
+}
+
+inherit(ModelStore, Super);
+
+ModelStore.prototype.set = function(model) {
+  var existingAttrs, id, key, modelName, newAttrs;
+
+  id = model.get(model.idAttribute);
+  modelName = modelUtils.modelName(model.constructor);
+  if (modelName == null) {
+    throw new Error('Undefined modelName for model');
   }
+  key = getModelStoreKey(modelName, id);
 
-  ModelStore.prototype.set = function(model) {
-    var existingAttrs, id, key, modelName, newAttrs;
+  /*
+  * We want to merge the model attrs with whatever is already
+  * present in the store.
+  */
+  existingAttrs = this.get(modelName, id) || {};
+  newAttrs = _.extend({}, existingAttrs, model.toJSON());
+  return Super.prototype.set.call(this, key, newAttrs, null);
+};
 
-    id = model.get(model.idAttribute);
-    modelName = modelUtils.modelName(model.constructor);
-    if (modelName == null) {
-      throw new Error('Undefined modelName for model');
+ModelStore.prototype.get = function(modelName, id, returnModelInstance) {
+  var key, modelData;
+
+  if (returnModelInstance == null) {
+    returnModelInstance = false;
+  }
+  key = getModelStoreKey(modelName, id);
+  modelData = Super.prototype.get.call(this, key);
+  if (modelData) {
+    if (returnModelInstance) {
+      return modelUtils.getModel(modelName, modelData, {
+        app: this.app
+      });
+    } else {
+      return modelData;
     }
-    key = getModelStoreKey(modelName, id);
+  }
+};
 
-    /*
-    * We want to merge the model attrs with whatever is already
-    * present in the store.
-    */
-    existingAttrs = this.get(modelName, id) || {};
-    newAttrs = _.extend({}, existingAttrs, model.toJSON());
-    return ModelStore.__super__.set.call(this, key, newAttrs, null);
-  };
-
-  ModelStore.prototype.get = function(modelName, id, returnModelInstance) {
-    var key, modelData;
-
-    if (returnModelInstance == null) {
-      returnModelInstance = false;
-    }
-    key = getModelStoreKey(modelName, id);
-    modelData = ModelStore.__super__.get.call(this, key);
-    if (modelData) {
-      if (returnModelInstance) {
-        return modelUtils.getModel(modelName, modelData, {
-          app: this.app
-        });
-      } else {
-        return modelData;
-      }
-    }
-  };
-
-  ModelStore.prototype._formatKey = function(key) {
-    return ModelStore.__super__._formatKey.call(this, "_ms:" + key);
-  };
-
-  return ModelStore;
-
-})(MemoryStore);
+ModelStore.prototype._formatKey = function(key) {
+  return Super.prototype._formatKey.call(this, "_ms:" + key);
+};
 
 function getModelStoreKey(modelName, id) {
   var underscored = modelUtils.underscorize(modelName);

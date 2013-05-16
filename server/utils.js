c081baae69ef6269ad455e10724f67c9dbdaa4c6
Joseph (Jos) Smith <jsmith@itriagehealth.com>
2013-05-15 22:25:27 -0600

    adding walk util

--- server/utils.js
+++ server/utils.js
@@ -1,6 +1,7 @@
-var utils, _;
+var utils, _, fs;
 
 _ = require('underscore');
+fs = require('fs');
 
 utils = module.exports = {};
 
@@ -15,4 +16,30 @@ utils.isErrorStatus = function(statusCode, options) {
   } else {
     return statusCode >= 400 && statusCode < 600;
   }
+};
+
+utils.walk = function(dir, callback) {
+  var results = [];
+
+  fs.readdir(dir, function(err, list) {
+    if (err) return callback(err);
+
+    var pending = list.length;
+    if (!pending) return callback(null, results);
+
+    list.forEach(function(file) {
+      file = dir + '/' + file;
+      fs.stat(file, function(err, stat) {
+        if (stat && stat.isDirectory()) {
+          walk(file, function(err, res) {
+            results = results.concat(res);
+            if (!--pending) { callback(null, results); }
+          });
+        } else {
+          results.push(file);
+          if (!--pending) { callback(null, results); }
+        }
+      });
+    });
+  });
 };

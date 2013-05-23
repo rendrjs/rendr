fa1786d8eb03719aec965a9aaf66141a5c513d33
Spike Brehm <ocelot@gmail.com>
2013-05-22 19:26:38 -0700

    Override 'each' Handlebars helper to pass in special context

--- shared/handlebarsHelpers.js
+++ shared/handlebarsHelpers.js
@@ -10,19 +10,22 @@ modelUtils = null;
 
 module.exports = {
   view: function(viewName, block) {
-    var ViewClass, app, html, options, view;
+    var ViewClass, html, options, view;
 
     BaseView = BaseView || require('./base/view');
     modelUtils = modelUtils || require('./modelUtils');
     viewName = modelUtils.underscorize(viewName);
     options = block.hash || {};
-    app = this._app;
-    if (app != null) {
-      options.app = app;
+
+    // Pass through a reference to the app.
+    if (this._app) {
+      options.app = this._app;
     }
 
     // Pass through a reference to the parent view.
-    options.parentView = this._view;
+    if (this._view) {
+      options.parentView = this._view;
+    }
 
     // get the Backbone.View based on viewName
     ViewClass = BaseView.getView(viewName);
@@ -47,5 +50,42 @@ module.exports = {
 
   json: function(object) {
     return new Handlebars.SafeString(JSON.stringify(object) || 'null');
+  },
+
+  /**
+   * Extend `each` to pass through important context. By default, `each` calls
+   * `options.fn` without a calling context.
+   */
+  each: function(context, options) {
+    var callingContext = getOptionsFromContext(this);
+    // Make sure `this._app`, `this._view`, etc are available.
+    options.fn = options.fn.bind(callingContext);
+    // Call the original helper with new context.
+    Handlebars.helpers.each.call(this, context, options);
   }
 };
+
+/**
+ * Grab important underscored properties from the current context.
+ * These properties come from BaseView::decorateTemplateData().
+ */
+function getOptionsFromContext(obj) {
+  var options, keys, value;
+
+  keys = [
+    '_app',
+    '_view',
+    '_model',
+    '_collection'
+  ];
+
+  options = keys.reduce(function(memo, key) {
+    value = obj[key];
+    if (value) {
+      memo[key] = value;
+    }
+    return memo;
+  }, {});
+
+  return options;
+}

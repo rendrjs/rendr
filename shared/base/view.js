19eb90df52e67c44f3e0a861bf740c9672df4bfd
Spike Brehm <ocelot@gmail.com>
2013-05-27 12:24:35 -0700

    Fix BaseView::getHtml()

--- shared/base/view.js
+++ shared/base/view.js
@@ -210,7 +210,7 @@ module.exports = BaseView = Backbone.View.extend({
 
     html = this.getInnerHtml();
     attributes = this.getAttributes();
-    attrString = attributes.reduce(function(memo, value, key) {
+    attrString = _.inject(attributes, function(memo, value, key) {
       return memo += " " + key + "=\"" + value + "\"";
     }, '');
     return "<" + this.tagName + attrString + ">" + html + "</" + this.tagName + ">";

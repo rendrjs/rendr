f2fb7751f433a29ee745e7469e41423b92866007
Spike Brehm <ocelot@gmail.com>
2013-07-16 13:54:00 -0700

    Add BaseApp#options and allow accessing Express req in postInitialize()

--- server/middleware/initApp.js
+++ server/middleware/initApp.js
@@ -1,12 +1,12 @@
 /*global rendr*/
 
 /**
- * We add 'req.rendrApp' so any middleware can access the Rendr
+ * We add `req.rendrApp` so any middleware can access the Rendr
  * app. We need to inject it into views, models, etc., in order
  * to provide user-specific functionality, such as sessions.
  * We can't just access it as a global, because there are concurrent
  * requests for different users.
-*/
+ */
 module.exports = function(appAttributes) {
   appAttributes = appAttributes || {};
   return function(req, res, next) {
@@ -14,14 +14,24 @@ module.exports = function(appAttributes) {
 
     App = require(rendr.entryPath + '/app/app');
 
-    // Pass any config that needs to be accessible by the client
-    // and server into the app.
-    app = new App(appAttributes);
+    /**
+     * Pass any data that needs to be accessible by the client
+     * and server into the app as `appAttributes`.
+     * Pass any non-app-data config using `appOptions`.
+     */
+    var appOptions = {
+      /**
+       * Hold on to a copy of the original request, so we can pull headers, etc.
+       * This will only be accessible on the server.
+       */
+      req: req
+    };
 
-    // Hold on to a copy of the original request, so we can pull headers, etc.
-    app.req = req;
+    app = new App(appAttributes, appOptions);
 
-    // Stash on the request so can be accessed elsewhere.
+    /**
+     * Stash the app instance on the request so can be accessed in other middleware.
+     */
     req.rendrApp = app;
 
     next();

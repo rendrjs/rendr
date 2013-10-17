c62f2ee4bb4e43dd599fdb1a7c17a93030ca5994
Thomas Shafer <thomasjshafer@gmail.com>
2013-10-16 17:14:24 -0700

    Made test helper for preparing client tests

--- test/client/router.test.js
+++ test/client/router.test.js
@@ -1,16 +1,12 @@
-var App, Backbone, BaseView, Router, should, sinon, routerConfig;
+var App, Backbone, BaseView, Router, should, routerConfig, clientTestHelper;
 
 Backbone = require('backbone');
 should = require('should');
-sinon = require('sinon');
 
 App = require('../../shared/app');
 BaseView = require('../../shared/base/view');
 Router = require('../../client/router');
-
-// This should go in a global setup.
-// it sets a global $ variable, which is accessed in AppView;
-Backbone.$ = global.$ = require('jquery').create();
+clientTestHelper = require('../helpers/client_test');
 
 routerConfig = {
   app: new App,
@@ -21,18 +17,8 @@ routerConfig = {
 
 describe("client/router", function() {
 
-  // Since this is simulating the client, we need to find the elements in the jsdom
-  before(function(){
-    this.originalEnsure = BaseView.prototype._ensureElement;
-    this.originalDelegate = BaseView.prototype.delegateEvents;
-    BaseView.prototype._ensureElement = Backbone.View.prototype._ensureElement;
-    BaseView.prototype.delegateEvents = Backbone.View.prototype.delegateEvents;
-  });
-
-  after(function(){
-    BaseView.prototype._ensureElement = this.originalEnsure;
-    BaseView.prototype.delegateEvents = this.originalDelegate;
-  });
+  before(clientTestHelper.before);
+  after(clientTestHelper.after);
 
   beforeEach(function() {
     this.router = new Router(routerConfig);
---
+++ test/helpers/client_test.js
@@ -0,0 +1,26 @@
+// Usage:
+// clientTestHelper = require('../helpers/client_test');
+// within describe:
+//  before(clientTestHelper.before);
+//  after(clientTestHelper.after);
+
+var Backbone, BaseView;
+
+Backbone = require('backbone');
+BaseView = require('../../shared/base/view');
+
+exports.before = function(){
+  this.originalBackbonejQuery = Backbone.$;
+  this.originalEnsure = BaseView.prototype._ensureElement;
+  this.originalDelegate = BaseView.prototype.delegateEvents;
+  BaseView.prototype._ensureElement = Backbone.View.prototype._ensureElement;
+  BaseView.prototype.delegateEvents = Backbone.View.prototype.delegateEvents;
+  Backbone.$ = global.$ = require('jquery').create();
+}
+
+exports.after = function(){
+  delete global.$;
+  Backbone.$ = this.originalBackbonejQuery;
+  BaseView.prototype._ensureElement = this.originalEnsure;
+  BaseView.prototype.delegateEvents = this.originalDelegate;
+}

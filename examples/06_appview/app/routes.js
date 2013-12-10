ad6461e147d439e59a53c60fcd268d571e446a7e
bigethan <e@bigethan.com>
2013-12-09 21:45:55 -0800

    moving appview stuff into it's own example, and removing the app_view stuff from the oher examples

---
+++ examples/06_appview/app/routes.js
@@ -0,0 +1,8 @@
+module.exports = function(match) {
+  match('',                   'home#index');
+  match('repos',              'repos#index');
+  match('repos/:owner/:name', 'repos#show');
+  match('users'       ,       'users#index');
+  match('users/:login',       'users#show');
+  match('users_lazy/:login',  'users#show_lazy');
+};

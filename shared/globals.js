0af37e21d18911040eadb7db5eee68c4526ad2ba
Spike Brehm <ocelot@gmail.com>
2013-12-10 18:03:43 -0800

    Finall move global.rendr

--- shared/globals.js
+++ shared/globals.js
@@ -1,12 +1,6 @@
 if (typeof window !== "undefined" && window !== null) {
   window.isServer = false;
   window.global = window;
-  global.rendr = {
-    entryPath: ''
-  };
 } else {
   global.isServer = true;
-  // hide it from requirejs since it's server only
-  var serverOnly_rendrIndex = '../index';
-  global.rendr = require(serverOnly_rendrIndex);
 }

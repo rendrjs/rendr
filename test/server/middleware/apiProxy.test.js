e03659bce9123650094485299491b6116c6ce73c
Christoph Neuroth <christoph.neuroth@gmail.com>
2013-12-12 18:12:18 +0100

    add unit test for apiProxy middleware

--- test/server/middleware/apiProxy.test.js
+++ test/server/middleware/apiProxy.test.js
@@ -1,8 +1,39 @@
 var _ = require('underscore'),
+    sinon = require('sinon'),
     apiProxy = require('./../../../server/middleware/apiProxy'),
     should = require('chai').should();
 
 describe('apiProxy', function() {
+
+  describe('middleware', function () {
+
+    var dataAdater, proxy, responseToClient;
+
+    beforeEach(function () {
+      dataAdater = { request: sinon.stub() },
+      proxy = apiProxy(dataAdater),
+      responseToClient = { status: sinon.spy(), json: sinon.spy() };
+    });
+
+    it('should pass through the status code', function () {
+      dataAdater.request.yields(null, {status: 200}, {});
+
+      proxy({ path: '/' }, responseToClient);
+
+      responseToClient.status.should.have.been.calledOnce;
+    });
+
+    it('should pass through the body', function () {
+      var body = { what: 'ever' };
+      dataAdater.request.yields(null, {status: 200}, body);
+
+      proxy({ path: '/' }, responseToClient);
+
+      responseToClient.json.should.have.been.calledOnce;
+      responseToClient.json.should.have.been.calledWith(body);
+    });
+
+  });
 
   describe('getApiPath', function() {
     it('should support no separator', function() {

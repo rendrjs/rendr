a2914c35679d1b130991864bb738c38da83aceb7
Mathias Schreck <mat@lusini.com>
2013-12-20 16:25:44 +0100

    remove unnecessary commas

--- examples/00_simple/Gruntfile.js
+++ examples/00_simple/Gruntfile.js
@@ -67,27 +67,27 @@ module.exports = function(grunt) {
     browserify: {
       basic: {
         src: [
-          'app/**/*.js',
+          'app/**/*.js'
         ],
         dest: 'public/mergedAssets.js',
         options: {
           debug: true,
           alias: [
-            'node_modules/rendr-handlebars/index.js:rendr-handlebars',
+            'node_modules/rendr-handlebars/index.js:rendr-handlebars'
           ],
           aliasMappings: [
             {
               cwd: 'app/',
               src: ['**/*.js'],
               dest: 'app/'
-            },
+            }
           ],
           shim: {
             jquery: {
               path: 'assets/vendor/jquery-1.9.1.min.js',
-              exports: '$',
-            },
-          },
+              exports: '$'
+            }
+          }
         }
       }
     }

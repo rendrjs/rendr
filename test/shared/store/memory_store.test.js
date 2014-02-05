cb3558fc5ea0064a86178959e1ef5e8e40917d53
Mathias Schreck <mat@lusini.com>
2014-02-05 14:46:55 +0100

    use closure instead of this context

--- test/shared/store/memory_store.test.js
+++ test/shared/store/memory_store.test.js
@@ -2,44 +2,44 @@ var MemoryStore = require('../../../shared/store/memory_store'),
     should = require('chai').should();
 
 describe('MemoryStore', function() {
+  var store;
+
   beforeEach(function() {
-    this.store = new MemoryStore;
+    store = new MemoryStore;
   });
 
   it("should undefined for missing keys", function() {
-    this.store.get('foobar', function(err, value) {
+    store.get('foobar', function(err, value) {
       should.equal(value, void 0);
     });
   });
 
   it("should set and get a key + value", function() {
-    this.store.set('cached_value', 42);
-    this.store.get('cached_value', function(err, value) {
+    store.set('cached_value', 42);
+    store.get('cached_value', function(err, value) {
       should.equal(value, 42);
     });
-    this.store.set('cached_value', 'new value');
-    this.store.get('cached_value', function(err, value) {
+    store.set('cached_value', 'new value');
+    store.get('cached_value', function(err, value) {
       should.equal(value, 'new value');
     });
   });
 
   it("should be able to clear a key", function() {
-    this.store.set('somethin', 'some value');
-    this.store.clear('somethin');
-    this.store.get('somethin', function(err, value) {
+    store.set('somethin', 'some value');
+    store.clear('somethin');
+    store.get('somethin', function(err, value) {
       should.equal(value, void 0);
     });
   });
 
   it("should be able to expire a key", function(done) {
-    var _this = this;
-
-    this.store.set('will_expire', '1234', 0.01);
-    this.store.get('will_expire', function(err, value) {
+    store.set('will_expire', '1234', 0.01);
+    store.get('will_expire', function(err, value) {
       should.equal(value, '1234');
     });
     setTimeout(function() {
-      _this.store.get('will_expire', function(err, value) {
+      store.get('will_expire', function(err, value) {
         should.equal(value, void 0);
       });
       done();

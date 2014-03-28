4f2c96b8233830430eee5841b34a9f9cd47e9a34
dettier <dettier.assets@gmail.com>
2014-03-28 15:43:33 +0600

    Gulp example

---
+++ examples/07_gulp/app/controllers/repos_controller.js
@@ -0,0 +1,21 @@
+module.exports = {
+  index: function(params, callback) {
+
+    var spec = {
+      collection: {collection: 'Repos', params: params}
+    };
+    this.app.fetch(spec, function(err, result) {
+      callback(err, result);
+    });
+  },
+
+  show: function(params, callback) {
+    var spec = {
+      model: {model: 'Repo', params: params, ensureKeys: ['language', 'watchers_count']},
+      build: {model: 'Build', params: params}
+    };
+    this.app.fetch(spec, function(err, result) {
+      callback(err, result);
+    });
+  }
+};
---
+++ examples/07_gulp/test/app/models/user.test.js
@@ -0,0 +1,10 @@
+var User = require('../../../app/models/user');
+
+describe('User', function() {
+
+  it('should use login as the model id', function() {
+    var user = new User({login: 'someusername'});
+    user.id.should.equal('someusername');
+  });
+
+});

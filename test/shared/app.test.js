f9324002778f602071e5cc9879e53abe1263762c
Jon Merrifield <jon@jmerrifield.com>
2015-03-24 11:02:32 -0800

    Test coverage for new functionality

--- test/shared/app.test.js
+++ test/shared/app.test.js
@@ -26,4 +26,41 @@ describe('BaseApp', function() {
       new MyApp();
     });
   });
+
+  describe('constructor', function() {
+    context('with a custom templateAdapter module name', function() {
+      beforeEach(function () {
+        this.attributes = {templateAdapter: '../test/fixtures/app/template_adapter'};
+      });
+
+      it('creates the templateAdapter we specify', function() {
+        var app = new App(this.attributes);
+
+        expect(app.templateAdapter).to.have.property('name', 'Test template adapter');
+      });
+
+      it('supplies the entryPath to the template adapter', function() {
+        var app = new App(this.attributes, {entryPath: 'myEntryPath'});
+
+        expect(app.templateAdapter).to.have.deep.property('suppliedOptions.entryPath', 'myEntryPath');
+      });
+    });
+
+    context('with a concrete templateAdapterInstance', function() {
+      it('uses the supplied templateAdapterInstance', function() {
+        var myTemplateAdapter = {};
+        var app = new App(null, {templateAdapterInstance: myTemplateAdapter});
+
+        expect(app.templateAdapter).to.equal(myTemplateAdapter);
+      });
+
+      it('does not try to require a template adapter by name', function () {
+        new App({
+          templateAdapter: 'non existent module name - should throw'
+        }, {
+          templateAdapterInstance: {}
+        });
+      });
+    });
+  });
 });
